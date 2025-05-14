999482c1773a58a05e502733d839e0836f921ffe
Tanzim <shahrier@iut-dhaka.edu>
2025-05-14 17:28:41 +0600

    Latest working version

--- iut-siks/models/Event.js
+++ iut-siks/models/Event.js
@@ -1,27 +1,3 @@
-<<<<<<< Updated upstream
-import mongoose from "mongoose";
-
-const eventSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    date: {
-        type: Date,
-        required: true,
-    },
-    description: {
-        type: String,
-        required: true,
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
-});
-const Event = mongoose.models.Event || mongoose.model("Event", eventSchema);
-export default Event;
-=======
 import mongoose from 'mongoose';
 
 const EventSchema = new mongoose.Schema({
@@ -31,4 +7,3 @@ const EventSchema = new mongoose.Schema({
 });
 
 export default mongoose.models.Event || mongoose.model('Event', EventSchema);
->>>>>>> Stashed changes
